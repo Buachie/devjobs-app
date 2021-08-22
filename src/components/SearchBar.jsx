bbd8b0584f766ac528342ce6d40026ba0a160c07
Aaron Buachie <aaron.buachiea@gmail.com>
2021-08-22 11:40:15 -0400

    Add padding to company logos, add checked state for fulltime listings

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useRef } from "react";
 import { useSearch } from "../contexts/SearchContext";
 import SearchIcon from "../assets/desktop/icon-search.svg";
+import WhiteSearchIcon from "../assets/desktop/search-white.svg";
 import LocationIcon from "../assets/desktop/icon-location.svg";
 import FilterIcon from "../assets/mobile/icon-filter.svg";
 
@@ -8,38 +9,70 @@ const SearchBar = () => {
   const searchRef = useRef();
   const locationRef = useRef();
   const fullTimeRef = useRef();
+  const searchMobileRef = useRef();
+  const locationMobileRef = useRef();
+  const fullTimeMobileRef = useRef();
   const { setFilters } = useSearch();
-
   const [modal, setModal] = useState(false);
+  const [isChecked, setIsChecked] = useState(true);
+
+  const handleCheck = () => {
+    setIsChecked(!isChecked);
+    console.log(isChecked);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    await setFilters(
-      searchRef.current.value,
-      locationRef.current.value,
-      fullTimeRef.current.value
-    );
+    if (isChecked) {
+      await setFilters(
+        searchRef.current.value,
+        locationRef.current.value,
+        fullTimeRef.current.value,
+        searchMobileRef.current.value,
+        locationMobileRef.current.value,
+        fullTimeMobileRef.current.value
+      );
+    } else {
+      await setFilters(
+        searchRef.current.value,
+        locationRef.current.value,
+        null,
+        searchMobileRef.current.value,
+        locationMobileRef.current.value,
+        null
+      );
+    }
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    setModal(true);
   };
 
   const mobileMenu = (
-    <form className="mobile-search" onSubmit={handleSubmit}>
+    <form className="mobile-search">
       <div className="input-container">
         <input
           type="text"
           name="title"
-          id="title"
+          id="title-mobile"
           className="text-input"
-          placeholder="Filter by title, companies..."
-          ref={searchRef}
+          placeholder="Filter by title or company"
+          ref={searchMobileRef}
         />
       </div>
       <div className="filters">
-        <img
-          src={FilterIcon}
-          alt="filter-icon"
-          onClick={() => setModal(true)}
-        />
-        <input type="submit" className="button dark" />
+        <button className="filter-button" onClick={handleClick}>
+          <img src={FilterIcon} alt="filter-icon" />
+        </button>
+
+        <button className="button">
+          <img
+            src={WhiteSearchIcon}
+            alt="white-search-icon"
+            onClick={handleSubmit}
+          />
+        </button>
       </div>
     </form>
   );
@@ -56,19 +89,19 @@ const SearchBar = () => {
           <input
             type="text"
             name="location"
-            id="location"
+            id="location-mobile"
             className="text-input"
             placeholder="Filter by location"
-            ref={locationRef}
+            ref={locationMobileRef}
           />
         </div>
         <div className="input-container fullTime">
           <input
             type="checkbox"
             name="fulltime"
-            id="fulltime"
+            id="fulltime-mobile"
             value="Full Time"
-            ref={fullTimeRef}
+            ref={fullTimeMobileRef}
           />
           <h3>Fulltime Only</h3>
         </div>
@@ -91,7 +124,7 @@ const SearchBar = () => {
             name="title"
             id="title"
             className="text-input"
-            placeholder="Filter by title, companies..."
+            placeholder="Filter by title or company"
             ref={searchRef}
           />
         </div>
@@ -112,8 +145,10 @@ const SearchBar = () => {
               type="checkbox"
               name="fulltime"
               id="fulltime"
-              value="Full Time"
+              value={false}
               ref={fullTimeRef}
+              checked={isChecked}
+              onChange={handleCheck}
             />
             <h3>Fulltime Only</h3>
           </div>
--- src/contexts/SearchContext.jsx
+++ src/contexts/SearchContext.jsx
@@ -11,10 +11,28 @@ export const SearchProvider = ({ children }) => {
   const [location, setLocation] = useState(null);
   const [fullTime, setFullTime] = useState(null);
 
-  const setFilters = (queryInput, locationInput, fullTimeInput) => {
-    setTitle(queryInput);
-    setLocation(locationInput);
-    setFullTime(fullTimeInput);
+  const setFilters = (
+    queryInput,
+    locationInput,
+    fullTimeInput,
+    mobileQueryInput,
+    mobileLocationInput,
+    mobileFullTimeInput
+  ) => {
+    if (queryInput) setTitle(queryInput);
+    if (locationInput) setLocation(locationInput);
+    if (fullTimeInput) {
+      setFullTime(fullTimeInput);
+    } else {
+      setFullTime(null);
+    }
+    if (mobileQueryInput) setTitle(mobileQueryInput);
+    if (mobileLocationInput) setLocation(mobileLocationInput);
+    if (mobileFullTimeInput) {
+      setFullTime(mobileFullTimeInput);
+    } else {
+      setFullTime(null);
+    }
     console.log(title, location, fullTime);
   };
 
